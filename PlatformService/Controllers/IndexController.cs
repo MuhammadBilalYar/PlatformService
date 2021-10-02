77984112e7c52ccc83e3d428f46281a456f2438c
Muhammad.Bilal <mby01@hotmail.com>
2021-10-02 12:12:52 +0500

    resolve docker issue by disabling ssk redirection and set swagger as default page

---
+++ PlatformService/Controllers/IndexController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace PlatformService.Controllers
+{
+
+    public class IndexController : Controller
+    {
+        [Route(""), HttpGet]
+        [ApiExplorerSettings(IgnoreApi = true)]
+
+        public RedirectResult Index()
+        {
+            return Redirect("/swagger/");
+        }
+    }
+}
--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -43,14 +43,13 @@ namespace PlatformService
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PlatformService v1"));
-            }
 
-            app.UseHttpsRedirection();
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PlatformService v1"));
+
+
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
